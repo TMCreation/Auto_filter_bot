916e607638d729b15b53a353f59a3ab1d99d0888
TMCreation <87004839+TMCreation@users.noreply.github.com>
2022-08-04 22:26:23 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2,7 +2,7 @@
 import asyncio
 import re
 import ast
-
+import math
 from pyrogram.errors.exceptions.bad_request_400 import MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty
 from script import Script
 import pyrogram
